b38208dad86df42497f08da9b4c44e517521303e
Atila Machan <atiladev@abv.bg>
2025-03-15 23:22:35 +0200

    type aliases

--- dist/print.js
+++ dist/print.js
@@ -1,9 +1,9 @@
 "use strict";
-function calculateTax(income, taxYear = 2022) {
-    if (taxYear < 2022) {
-        return income * 1.2;
-    }
-    return income * 1.3;
-}
-calculateTax(10000, 2021);
+let employee = {
+    id: 1,
+    name: 'Atila',
+    retire: (date) => {
+        console.log(date);
+    },
+};
 //# sourceMappingURL=print.js.map
