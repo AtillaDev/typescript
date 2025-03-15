883dadd8fce8e5982d7e8abe9e74005a8ee330c7
Atila Machan <atiladev@abv.bg>
2025-03-15 23:39:36 +0200

    intersection types

--- src/print.ts
+++ src/print.ts
@@ -1,11 +1,14 @@
-function kgToLbs(weight: number | string): number {
-  // Narrowing
-  if (typeof weight === 'number') {
-    return weight * 2.2;
-  } else {
-    return parseInt(weight) * 2.2;
-  }
-}
+type Draggable = {
+  drag: () => void;
+};
 
-kgToLbs(10);
-kgToLbs('10kg');
+type Resizeble = {
+  resize: () => void;
+};
+
+type UIWidget = Draggable & Resizeble;
+
+let textBox: UIWidget = {
+  drag: () => {},
+  resize: () => {},
+};
